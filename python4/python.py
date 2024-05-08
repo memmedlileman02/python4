f0d72777c36010364ec625100c5ec1f07012fa9e
memmedlileman02 <161588533+memmedlileman02@users.noreply.github.com>
2024-05-08 12:44:24 +0400

    Add files via upload

---
+++ python4/python.py
@@ -0,0 +1,102 @@
+# Inheritance 
+# class chess():
+#     def __init__(self):
+#         print("sahmat yaradildi")
+
+#     def top(self):
+#         print("şaquli və üfüqi xanalar üzrə hərəkət edir")
+
+#     def at(self):
+#         print(" L seklinde hereket edir")
+
+#     def vezir(self):
+#         print("tutduğu xanadan şaquli, üfüqi və ya diaqonal xanalar üzrə hərəkət edi")
+
+#     def fil(self):
+#         print(" ancaq diaqonal xanalarla hərəkət edir")     
+
+#     def piyada(self):
+#         print(" ancaq qabağa hərəkət edə bilərlər")   
+
+
+# class Sehriyar_Memmedyarov(chess):
+#     def __init__(self):
+#         super().__init__()
+#         print(" kisi sahmatcilar")
+    
+#     def FIDE_ustasi(self):
+#         print("kisi FIDE ustasidir")
+
+#     def azerbaycan_cempionati(self):
+#         print("kisi azerbaycan cempionudur")
+
+#     def qroyssmester(self):
+#         print("kisi qroyssmesterdir")
+
+# Sehriyar_Memmedyarov = Sehriyar_Memmedyarov()
+# Sehriyar_Memmedyarov.qroyssmester()
+# Sehriyar_Memmedyarov.FIDE_ustasi()
+# Sehriyar_Memmedyarov.azerbaycan_cempionati()
+
+
+
+# Polymorphism 
+# class chess():
+#     def __init__(self):
+#         print("sahmat yaradildi")
+
+#     def top(self):
+#         print("şaquli və üfüqi xanalar üzrə hərəkət edir")
+
+#     def at(self):
+#         print(" L seklinde hereket edir")
+
+#     def vezir(self):
+#         print("tutduğu xanadan şaquli, üfüqi və ya diaqonal xanalar üzrə hərəkət edi")
+
+#     def fil(self):
+#         print(" ancaq diaqonal xanalarla hərəkət edir")     
+
+#     def piyada(self):
+#         print(" ancaq qabağa hərəkət edə bilərlər")   
+
+
+# class Sehriyar_Memmedyarov(chess):
+#     def __init__(self):
+#         super().__init__()
+#         print(" kisi sahmatcilar")
+    
+#     def FIDE_ustasi(self):
+#         print("kisi FIDE ustasidir")
+
+#     def azerbaycan_cempionati(self):
+#         print("kisi azerbaycan cempionudur")
+
+#     def qroyssmester(self):
+#         print("kisi qroyssmesterdir")
+
+
+# class Nərmin_Kazimova(chess):
+#     def __init__(self):
+#         super().__init__()
+#         print(" qadin sahmatcilar")
+    
+#     def FIDE_ustasi(self):
+#         print("qadin FIDE ustasidir")
+
+#     def azerbaycan_cempionati(self):
+#         print("qadin azerbaycan cempionu")
+
+#     def qroyssmester(self):
+#         print("qadin qroyssmester")
+
+
+# sahmat = chess()
+# Sehriyar_Memmedyarov = Sehriyar_Memmedyarov()
+# Nərmin_Kazimova = Nərmin_Kazimova()
+
+
+
+
+         
+        
